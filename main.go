bd05146d1a5997112325dd44408cf35dddd8aa64
lhz <pepitsizous@zoho.com>
2021-03-28 22:03:36 +0300

    make main() a tiny bit better

--- main.go
+++ main.go
@@ -4,17 +4,26 @@ import (
 	"go-usrv-tmpl/domain"
 	mockrepo "go-usrv-tmpl/repo/mock"
 	"log"
+	"os"
 )
 
-func chooseRepo() domain.ThingRepo {
+func chooseRepo() (domain.ThingRepo, error) {
 	repo, err := mockrepo.NewMockRepo()
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
-	return repo
+	return repo, nil
 }
 
 func main() {
-	repo := chooseRepo()
-	log.Println("main() still in primitive state but here's a repo", repo)
+	repo, err := chooseRepo()
+	if err != nil {
+		log.Fatalln("error with repository:", err)
+	}
+	log.Println("repo", repo, "selected")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "53011"
+	}
+	log.Println("port", port, "selected")
 }
